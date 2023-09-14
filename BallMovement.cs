16618107d29d3d68e51d93eb55386192a0075d7d
TheyHateLost <144147218+TheyHateLost@users.noreply.github.com>
2023-09-14 11:14:10 -0400

    Add files via upload

--- BallMovement.cs
+++ BallMovement.cs
@@ -4,49 +4,85 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    //variable for speed 
-    public float xSpeed = 0; //variable for x speed 
-    private float xBorder = 0; //variable for y speed 
+    //variable for speed
+    public float xSpeed = 0; //variable for horizontal speed
+    public float ySpeed = 0; //variable for vertical speed
 
-    //variable borders 
-    private float xBorder = 7.5f; //variable for horizonal border 
-    private float yborder = 4.5f; //variable for vertical border 
+    //variable for borders
+    private float xBorder = 7.5f; //variable for horizontal border
+    private float yBorder = 4.5f; //variable for vertical border
 
-    //variable for move state 
-    public bool xMove = true; 
-    public bool yMove = true; 
-    
-    
-    //start is called before the first frame update 
-    void Start()...
+    //variable for move state
+    public bool xMove = true;
+    public bool yMove = true;
 
-        //update is called once per frame 
-        void Update ()
 
     // Start is called before the first frame update
     void Start()
     {
-        xSpeed = 0.0125f; //declare value for horizonal speed 
-        yspeed = 0.0125f; // declare value for vertical speed 
+        xSpeed = 0.0125f;   //declare value for horizontal speed
+        ySpeed = 0.0125f;   //declare value for vertical speed
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-        //horizonal movement
-        transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); //move right 
-      else (yMove == true) 
-        if (transform.position.x >= xBorder)
-
+        //horizontal movement
+        if (xMove == true)
         {
-            xSpeed = -xSpeed; //make it go left 
+            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y); //move right
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y); //move left
+        }
 
-            if (transforms.position.x <= -xBorder)
 
+        if (transform.position.x >= xBorder)
+        {
+            xMove = false; //make it go left
+        }
+
+        if (transform.position.x <= -xBorder)
+        {
+            xMove = true;
+        }
+
+        //vertical movement
+        if (yMove == true)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed); //move up
+        }
+
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed);   //move down
+        }
+        if (transform.position.y >= yBorder)
+        {
+            yMove = false;
+        }
+
+        if (transform.position.y <= -yBorder)
+        {
+            yMove = true;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) //when a collision happens
+    {
+        Debug.Log("hit");
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (xMove == true)
             {
-                xSpeed *= -1;
+                xMove = false;
             }
-
-
+            else if (xMove == false)
+            {
+                xMove = true;
+            }
+        }
+    }
+}
